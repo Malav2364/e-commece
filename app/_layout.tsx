efc64b98ea22b1a4aca9ec698a90a24fd6cbdfb0
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2024-12-09 15:54:17 +0530

    added tabs for routing

---
+++ app/_layout.tsx
@@ -0,0 +1,6 @@
+//layout is rendered first and then it renders the children components 
+import { Slot, Stack, Tabs } from "expo-router";
+
+export default function RootLayout(){
+    return <Tabs />;
+}
