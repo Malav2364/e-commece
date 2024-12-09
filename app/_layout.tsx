80cf3bec844934eed6bf0ad44ea8ba4e6f5fee0c
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2024-12-09 16:03:09 +0530

    Route using stack to get screen status on top

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,9 @@
 //layout is rendered first and then it renders the children components 
-import { Slot, Stack, Tabs } from "expo-router";
+//tabs are to display the tabs on the bottom of the screen
+//stack is to display the stack of the screens
+//slot is to display the children components
+import { Stack } from "expo-router"; //tabs are used for tab based navigation like whatsapp 
 
 export default function RootLayout(){
-    return <Tabs />;
+    return <Stack/>;
 }
