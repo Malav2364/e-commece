d3748e98b36e6353b0ef4b8f33dc933f9ca04128
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2024-12-11 14:40:39 +0530

    Gluestack Ui added and tested

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,14 @@
 //tabs are to display the tabs on the bottom of the screen
 //stack is to display the stack of the screens
 //slot is to display the children components
-import { Stack, Tabs } from "expo-router"; //tabs are used for tab based navigation like whatsapp 
+import { Stack } from "expo-router"; //tabs are used for tab based navigation like whatsapp 
+import { GluestackUIProvider } from "@/Components/ui/gluestack-ui-provider";
+
 
 export default function RootLayout(){
-    return <Tabs/>;
+    return (
+        <GluestackUIProvider>
+            <Stack/>
+        </GluestackUIProvider>
+    );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,31 @@
-import { Text, StyleSheet, FlatList } from "react-native";
-import products from '../assets/products.json'
-import ProductListItem from "../Components/ProductListItem";
+import { Text, StyleSheet, FlatList, View } from "react-native";
+// import products from '../assets/products.json'
+// import ProductListItem from "../Components/ProductListItem";
+import { Button, ButtonText } from "@/Components/ui/button";
+import "@/global.css";
+import { Box } from "@/Components/ui/box";
+import { Center } from "@/Components/ui/center";
 
 export default function HomeScreen() {
-  return (
-      <FlatList 
-          data = {products}
-          renderItem = {({item})=><ProductListItem product={item}/>}
-        />
+  return(
+    <Center className="flex-1 bg-green-200">
+      <Box>
+        <Text className="font-semibold text-9xl color-white">This is green bro</Text>
+      </Box>
+    </Center>
   );
+  // return (
+  //     <FlatList 
+  //         data = {products}
+  //         renderItem = {({item})=><ProductListItem product={item}/>}
+  //       />
+  // );
 }
 
 const styles = StyleSheet.create({
   container:{
       flex: 1,
-      backgroundColor: '#fff',
+      // backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center',
   },
