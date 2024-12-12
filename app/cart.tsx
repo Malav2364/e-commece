89b8f086ab186a64eec144f861e7c95a3e92e256
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2024-12-12 09:55:53 +0530

    idk the change

--- app/cart.tsx
+++ app/cart.tsx
@@ -1,10 +1,10 @@
 import { View, Text, StyleSheet } from "react-native";
 import AntDesign from '@expo/vector-icons/AntDesign';
+
 export default function Cart(){
     return(
         <View style={styles.container}>
-            <Text style={styles.title}>Cart Screen</Text>
-            <AntDesign name="shoppingcart" size={60} color="black" />
+            <Text style={styles.title}><AntDesign name="shoppingcart" size={50} color="black" />  is Empty</Text>
         </View>
     );
 }
