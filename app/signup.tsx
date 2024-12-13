e83dfc493dfb550300bda3598f49008a237d9d3a
Ayush Modi <2303031057006@paruluniversity.ac.in>
2024-12-13 17:52:38 +0530

    updated signup page

--- app/signup.tsx
+++ app/signup.tsx
@@ -1,26 +1,35 @@
+import { useFonts } from "expo-font";
 import { View, Text, StyleSheet } from "react-native";
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Image } from "@/Components/ui/image";
 import { Center } from "@/Components/ui/center";
 import { Box } from "@/Components/ui/box";
-Box
-
+import { Heading } from "@/Components/ui/heading";
+import { VStack } from "@/Components/ui/vstack";
+import { Input, InputField, InputIcon, InputSlot } from "@/Components/ui/input";
+import { Button, ButtonText } from "@/Components/ui/button";
 export default function SignUp() {
     return (
-        <Center className="flex-1 gap-6">
-            <Box className="border p-10 rounded-full">
-                <Image
-                    size="xl"
-                    source={{
-                        uri:"https://cdn4.iconfinder.com/data/icons/logos-brands-7/512/google_logo-google_icongoogle-512.png",
-                    }}
-                    alt="image"
-                />
-            </Box>
-            <Box className="border p-10 rounded-xl bg-yellow-200 italic">
-                <Text className="text-2xl color-green-300">Google kare aur fark jane</Text>
-            </Box>
-        </Center>
+        <Box className="bg-white flex-auto justify-evenly">
+            <VStack className="w-full p-4">        
+                <Heading className="text-4xl font-bold justify-between">Getting Started</Heading>
+                <Text className="text-xl text-slate-500">Sign Up to SustainFit</Text>
+            </VStack>
+            <VStack className="max-w-96">
+            <Input className="text-center" variant="rounded" size="lg" isDisabled={false} isInvalid={false} isReadOnly={false}>
+                <InputField placeholder="Enter your email" />
+            </Input>
+            </VStack>
+            <VStack className="max-w-96">
+            <Input className="text-center" variant="rounded" size="lg" isDisabled={false} isInvalid={false} isReadOnly={false}>
+                <InputField placeholder="Create a username" />
+            </Input>
+            </VStack>
+            <VStack className="max-w-96">
+                <Button size="md" variant="solid" action="primary" className="bg-green-400 ">
+                <ButtonText>Sign Up</ButtonText>
+                </Button>
+            </VStack>
+        </Box>
     );
 }
 
