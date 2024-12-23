0598a68b01214f8bb6895328e33a0b128f0884f2
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2024-12-23 09:53:12 +0530

    toast

--- app/cart.tsx
+++ app/cart.tsx
@@ -1,10 +1,21 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ToastAndroid} from "react-native";
 import AntDesign from '@expo/vector-icons/AntDesign';
+import { Button, ButtonText } from "@/Components/ui/button";
+
+
 
 export default function Cart(){
+
+    const showToast = ()=>{
+        ToastAndroid.show("Added to Cart !", ToastAndroid.SHORT);
+    }
+
     return(
         <View style={styles.container}>
             <Text style={styles.title}><AntDesign name="shoppingcart" size={50} color="black" />  is Empty</Text>
+            <Button className="bg-green-400" >
+                <ButtonText onPress={showToast} className="bg-green-400">Add to Cart</ButtonText>
+            </Button>
         </View>
     );
 }
